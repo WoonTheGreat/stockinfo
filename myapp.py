f5967c679bb4145be51bc7c2085760b98686d1ea
Woon TheGreat <94958932+WoonTheGreat@users.noreply.github.com>
2021-11-24 23:04:41 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import streamlit as st
+import pandas as pd
 
 st.write("""
 ## TSLA analysis
@@ -11,8 +12,11 @@ tickerSymbol = 'TSLA'
 # get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 # get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-10-31')
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-12-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
+df1 = tickerData.recommendations
+df2 = tickerData.news
+
 
 # tickerData.info
 # info on the company
@@ -20,11 +24,12 @@ tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-10-31')
 st.write("""
 ## TSLA news
 """)
-tickerData.news
+st.table(df2)
+
 st.write("""
 ## Recommendation by experts
 """)
-tickerData.recommendations
+df1
 # get recommendation data for ticker
 st.write("""
 ## Closing Price
